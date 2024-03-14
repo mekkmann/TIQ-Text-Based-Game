3b6d123d4ca85e629bfa0bb5c74c4e2841944db7
Pontus (Alexander) Liljekvist <alex.p.liljekvist@gmail.com>
2024-03-14 15:25:21 +0100

    intro
    - add intro
    - print intro
    - add functions to print lines and text files

--- Text-Based-Game/Program.cs
+++ Text-Based-Game/Program.cs
@@ -4,19 +4,54 @@
     {
         static void Main(string[] args)
         {
-            PrintTitle();
+            // print the title
+            PrintTextFile("title.txt", false);
+            // spacing
+            Console.WriteLine();
+            // print the intro lore
+            PrintTextFile("intro.txt", true);
 
             // to keep console open
             Console.ReadLine();
         }
 
-        static void PrintTitle()
+        /// <summary>
+        /// Prints the contents of a text file, line by line. As fast as possible or letter by letter.
+        /// </summary>
+        /// <param name="path">path to file</param>
+        /// <param name="letterByLetter">if the text should be printed "immediately" or letter by letter</param>
+        static void PrintTextFile(string path, bool letterByLetter)
         {
-            string[] fileLines = File.ReadAllLines("title.txt");
+            string[] fileLines = File.ReadAllLines(path);
 
-            foreach (string line in fileLines)
+            if (letterByLetter)
             {
-                Console.WriteLine(line);
+                foreach (string line in fileLines)
+                {
+                    PrintStringCharByChar(line);
+                }
+            }
+            else
+            {
+                foreach (string line in fileLines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Prints a string one character at a time
+        /// </summary>
+        static void PrintStringCharByChar(string line)
+        {
+            foreach (char c in line)
+            {
+                Console.Write(c);
+                //for testing
+                Thread.Sleep(10);
+                //for build
+                //Thread.Sleep(25);
             }
         }
     }
