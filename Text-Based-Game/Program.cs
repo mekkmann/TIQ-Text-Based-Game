3036dc5cf8e76a0fb60f661a2234ec770bb270ee
Pontus (Alexander) Liljekvist <alex.p.liljekvist@gmail.com>
2024-03-14 15:04:19 +0100

    Create project

---
+++ Text-Based-Game/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Text_Based_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
