75ddeba1f779b56b4b41e36469eb90ea83be2dc1
Pontus (Alexander) Liljekvist <alex.p.liljekvist@gmail.com>
2024-03-16 00:24:19 +0100

    start of enemy class + names

---
+++ Text-Based-Game/Classes/Enemy.cs
@@ -0,0 +1,20 @@
+﻿namespace Text_Based_Game.Classes
+{
+    internal class Enemy
+    {
+        public string Name { get; set; }
+        public float StatMultiplier { get; set; }
+        public float HP { get; set; }
+        public int MinDamage { get; set; }
+        public int MaxDamage { get; set; }
+        public float DodgeChance { get; set; }
+        public float XpDropped { get; set; }
+
+
+        // CONSTRUCTORS
+        public Enemy(string name)
+        {
+            Name = name;
+        }
+    }
+}
